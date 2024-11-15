69176f33267c395f31a5c2f6642a5d447362f1dd
mydimons <mydimons.kb@gmail.com>
2024-11-14 22:52:03 -0800

    more comments, reformatting
    also made a new function which should make adding more toppings that will need variation easier

--- games/pizza_clicker/pizza_clicker.py
+++ games/pizza_clicker/pizza_clicker.py
@@ -1,7 +1,7 @@
-import pygame, math, random
+import pygame, os, math, random
 pygame.font.init()
 
-#------Vars
+# ---Vars---
 
 #CLIENT
 WIDTH, HEIGHT = 1200, 1000
@@ -17,14 +17,12 @@ CHEESE_COLOR = 242, 217, 51
 
 #PIZZA
 PIZZA_RADIUS = 200
-PIZZA_POS_X = WIDTH/4
-PIZZA_POS_Y = HEIGHT/2
-PIZZA_POS = PIZZA_POS_X, PIZZA_POS_Y
+PIZZA_POS = WIDTH/4, HEIGHT/2
 
 #UPGRADES
 MULTIPLIER_ADD = 1
 UPGRADE_COST = 10
-UPGRADE_COST_INCREASE = 2.5
+UPGRADE_COST_INCREASE = 1.75
 UPGRADE_WIDTH = 450
 UPGRADE_HEIGHT = 75
 
@@ -32,23 +30,24 @@ UPGRADE_HEIGHT = 75
 SAUCE_RADIUS = PIZZA_RADIUS - 15
 CHEESE_RADIUS = PIZZA_RADIUS - 25
 PEPPERONI_RADIUS = PIZZA_RADIUS - 175
-PEPPERONI_VARIANCE = 20
+TOPPING_VARIANCE = 25
 
 SAUCE_UNLOCKED = False
 CHEESE_UNLOCKED = False
 PEPPERONI_UNLOCKED = False
 
 #CURRANCY
-MONEY = 0 #PEPS IS MAIN CURRANCY
+MONEY = 0 #MAIN CURRANCY
+
+# ---Vars---
 
 #DISPLAY
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("work pls")
 
-
-
 def main():
-    pepperoni_variance = calculate_pepperoni_variance()
+    # Calculate all the topping variation
+    pepperoni_variation = calculate_topping_variation()
     
     # Money
     money = 3000
@@ -78,6 +77,7 @@ def main():
         clock.tick(FPS)
         for event in pygame.event.get():
             mouse_pos = pygame.mouse.get_pos()
+            # ON CLICK
             if event.type == pygame.MOUSEBUTTONDOWN:
                 # Add money
                 if(clicked_circle(pizza, PIZZA_RADIUS)):
@@ -86,91 +86,117 @@ def main():
                     money_per_second_count += add_money
                     
                     print("---MONEY---")
+                    print("")
                     print(money)
+                    print("")
                     print("---MONEY---")
                     
-                # Increase money and add new topping on upgrade click
+                # Upgrade clicking
                 if upgrade_multiplier.collidepoint(mouse_pos):
                     if(money >= multiplier_cost):
                         money_muliplier += 1
                         money -= int(multiplier_cost)
+
                         multiplier_cost *= UPGRADE_COST_INCREASE
                         int(multiplier_cost)
+
                         toppings_unlocked += 1
+
                         print("---MULTIPLIER---")
+                        print("")
                         print(money_muliplier)
+                        print("")
                         print("---MULTIPLIER---")
                     else:
                         print("ERROR, NOT ENOUGH MONEY")
+
+            # Calculates the money per second
             elif event.type == money_per_second_timer:
-                money_per_second = money_per_second_count
+                money_per_second = money_per_second_count # (var changes when money is added)
                 money_per_second_count = 0
                 print("MONEY PER SECOND: " + str(money_per_second))
+
             if event.type == pygame.QUIT:
                 run = False
+        # Text
         money_text = font.render('$' + str(int(money)), False, WHITE)
         money_per_second_text = small_font.render('$ per second: ' + str(int(money_per_second)), False, WHITE)
         upgrade_text = small_font.render('NEXT TOPPING | COST: ' + str(int(multiplier_cost)), False, BLACK)
 
-        draw_window(money_text, upgrade_text, upgrade_multiplier, toppings_unlocked, money_per_second_text, pepperoni_variance)
+        draw_window(money_text           ,
+                    upgrade_text         , 
+                    upgrade_multiplier   , 
+                    toppings_unlocked    ,
+                    money_per_second_text, 
+                    pepperoni_variation  )
 
 
-def draw_window(money_text, upgrade_text, upgrade_multiplier_button, toppings_unlocked, money_per_second, pepperoni_variance): #UPDATE DISPLAY
+def draw_window(money_text, upgrade_text, upgrade_multiplier_button, toppings_unlocked, money_per_second, topping_variation): #UPDATE DISPLAY
     WIN.fill(BACKGROUND_COLOR)
-    #PIZZA
+
+    # PIZZA
     pygame.draw.circle(WIN, BROWN, PIZZA_POS, PIZZA_RADIUS)
-    #SAUCE
+
+    # SAUCE
     if toppings_unlocked >= 1:
         SAUCE_UNLOCKED = True
         pygame.draw.circle(WIN, SAUCE_COLOR, PIZZA_POS, SAUCE_RADIUS)
 
-    #CHEESE
+    # CHEESE
     if toppings_unlocked >= 2:
         CHEESE_UNLOCKED = True
         pygame.draw.circle(WIN, CHEESE_COLOR, PIZZA_POS, CHEESE_RADIUS)
 
+    # PEPPERONI
     if toppings_unlocked >= 3:
         PEPPERONI_UNLOCKED = True
-        pepperoni = [pygame.draw.circle(WIN, SAUCE_COLOR, (PIZZA_POS_X - 80  + pepperoni_variance[0][0], PIZZA_POS_Y  + 70 + pepperoni_variance[0][1]), PEPPERONI_RADIUS), 
-                     pygame.draw.circle(WIN, SAUCE_COLOR, (PIZZA_POS_X + 80  + pepperoni_variance[1][0], PIZZA_POS_Y  - 70 + pepperoni_variance[1][1]), PEPPERONI_RADIUS),
-                     pygame.draw.circle(WIN, SAUCE_COLOR, (PIZZA_POS_X + 80  + pepperoni_variance[2][0], PIZZA_POS_Y  + 70 + pepperoni_variance[2][1]), PEPPERONI_RADIUS),
-                     pygame.draw.circle(WIN, SAUCE_COLOR, (PIZZA_POS_X - 80  + pepperoni_variance[3][0], PIZZA_POS_Y  - 70 + pepperoni_variance[3][1]), PEPPERONI_RADIUS),
-                     pygame.draw.circle(WIN, SAUCE_COLOR, (PIZZA_POS_X       + pepperoni_variance[4][0], PIZZA_POS_Y + 125 + pepperoni_variance[4][1]), PEPPERONI_RADIUS), 
-                     pygame.draw.circle(WIN, SAUCE_COLOR, (PIZZA_POS_X       + pepperoni_variance[5][0], PIZZA_POS_Y - 125 + pepperoni_variance[5][1]), PEPPERONI_RADIUS),
-                     pygame.draw.circle(WIN, SAUCE_COLOR, (PIZZA_POS_X + 125 + pepperoni_variance[6][0], PIZZA_POS_Y       + pepperoni_variance[6][1]), PEPPERONI_RADIUS), 
-                     pygame.draw.circle(WIN, SAUCE_COLOR, (PIZZA_POS_X - 125 + pepperoni_variance[7][0], PIZZA_POS_Y       + pepperoni_variance[7][1]), PEPPERONI_RADIUS),
-                     pygame.draw.circle(WIN, SAUCE_COLOR, (PIZZA_POS_X       + pepperoni_variance[8][0], PIZZA_POS_Y       + pepperoni_variance[8][1]), PEPPERONI_RADIUS)]
+        render_variation_toppings(SAUCE_COLOR, PIZZA_POS[0], PIZZA_POS[1], topping_variation, PEPPERONI_RADIUS)
 
-
-    pygame.draw.rect(WIN, WHITE, pygame.Rect(upgrade_multiplier_button.x, upgrade_multiplier_button.y, upgrade_multiplier_button.width, upgrade_multiplier_button.height))
-
+    # Render text
     WIN.blit(money_per_second, (10, 80)) 
     WIN.blit(money_text, (10, 5))
-    upgrade_text_rect = upgrade_text.get_rect(center=(upgrade_multiplier_button.x + upgrade_text.get_rect().width/2 + 15, upgrade_multiplier_button.y + upgrade_text.get_rect().height))
+
+    # Render upgrade button
+    pygame.draw.rect(WIN, WHITE, pygame.Rect(upgrade_multiplier_button.x, upgrade_multiplier_button.y, upgrade_multiplier_button.width, upgrade_multiplier_button.height)) # upgrade button
+    upgrade_text_rect = upgrade_text.get_rect(center=(upgrade_multiplier_button.x + upgrade_text.get_rect().width/2 + 15, upgrade_multiplier_button.y + upgrade_text.get_rect().height)) # upgrade text
     WIN.blit(upgrade_text, upgrade_text_rect)
+
     pygame.display.update()
 
 def clicked_circle(self, radius):
     mouse_x, mouse_y = pygame.mouse.get_pos()
     dx = mouse_x - self.centerx
     dy = mouse_y - self.centery
-    distance = math.sqrt(dx*dx + dy*dy)  # or: distance = math.hypot(dx, dy)
+    distance = math.sqrt(dx*dx + dy*dy)
     return distance <= radius
 
-def calculate_pepperoni_variance():
+# Uses an array of turples to calculate the individual amount of variance for a topping
+def calculate_topping_variation():
     i = 0
-    pepperoni_variance = []
+    variance = []
     while i < 10:
-        pepperoni_variance.append([random.randrange(-PEPPERONI_VARIANCE, PEPPERONI_VARIANCE), random.randrange(-PEPPERONI_VARIANCE, PEPPERONI_VARIANCE)])
+        variance.append([random.randrange(-TOPPING_VARIANCE, TOPPING_VARIANCE), random.randrange(-TOPPING_VARIANCE, TOPPING_VARIANCE)])
         i += 1
 
-    return pepperoni_variance
+    return variance
+
+# CHANGE THIS FORMAT FROM CIRCLES -> IMAGES
+def render_variation_toppings(color, x_pos, y_pos, variation, radius):
+    pygame.draw.circle(WIN, color, (x_pos - 80  + variation[0][0], y_pos  + 70 + variation[0][1]), radius) 
+    pygame.draw.circle(WIN, color, (x_pos + 80  + variation[1][0], y_pos  - 70 + variation[1][1]), radius)
+    pygame.draw.circle(WIN, color, (x_pos + 80  + variation[2][0], y_pos  + 70 + variation[2][1]), radius)
+    pygame.draw.circle(WIN, color, (x_pos - 80  + variation[3][0], y_pos  - 70 + variation[3][1]), radius)
+    pygame.draw.circle(WIN, color, (x_pos       + variation[4][0], y_pos + 125 + variation[4][1]), radius) 
+    pygame.draw.circle(WIN, color, (x_pos       + variation[5][0], y_pos - 125 + variation[5][1]), radius)
+    pygame.draw.circle(WIN, color, (x_pos + 125 + variation[6][0], y_pos       + variation[6][1]), radius) 
+    pygame.draw.circle(WIN, color, (x_pos - 125 + variation[7][0], y_pos       + variation[7][1]), radius)
+    pygame.draw.circle(WIN, color, (x_pos       + variation[8][0], y_pos       + variation[8][1]), radius)
     
 if __name__ == "__main__":
     main()
 
-#TODO
+# TODO
 # (CHANGE) Center money text
 # (ADD)    MORE TOPPINGS
 # (ADD)    Money per second buildings
-# (ADD)    MORE COMMENTS
+# (CHANGE) Change rendering for pepperoni from circles to images, allowing easy addition of new toppings
